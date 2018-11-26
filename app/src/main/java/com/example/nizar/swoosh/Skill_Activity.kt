bbc14fc0f115ae4ec85fa4584b5d39799e55774f
nizar <nizar.amalaikah@hotmail.com>
2018-11-26 16:04:28 +0300

    added 3rd page and nav.

---
+++ app/src/main/java/com/example/nizar/swoosh/Skill_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.nizar.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Skill_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill_)
+    }
+}
